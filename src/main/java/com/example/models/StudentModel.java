ecd67bcd0134802b1a07c5f6fda3eae941bdcd02
Ashish <31588456+ashishkant@users.noreply.github.com>
2026-04-22 11:25:59 +0000

    hibernate

--- src/main/java/com/example/models/StudentModel.java
+++ src/main/java/com/example/models/StudentModel.java
@@ -1,10 +1,32 @@
 package com.example.models;
 
-class StudentModel {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class StudentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_roll_number")
     private int studentRollNumber;
+
+    @Column(name = "student_name")
     private String studentName;
+
+    @Column(name = "student_address")
     private String studentAddress;
+
+    @Column(name = "student_email")
     private String studentEmail;
+
+    // Default constructor required by Hibernate
+    public StudentModel() {
+    }
 
     public StudentModel(int studentRollNumber, String studentName, String studentAddress, String studentEmail) {
         this.studentRollNumber = studentRollNumber;
